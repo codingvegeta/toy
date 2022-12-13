98c271418c17765b8c262712a3e8f986a9beb114
codingvegeta <hong090992@gmail.com>
2022-12-13 21:15:41 +0900

    "feat : 페이지 컨트롤러를 구현하였습니다."

---
+++ kdt_coin/src/main/java/com/example/kdt_coin/page/controller/PageController.java
@@ -0,0 +1,39 @@
+package com.example.kdt_coin.page.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/goSignup")
+    public String signup() {
+        return "signup";
+    }
+
+    @GetMapping("/goLogin")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("goPat")
+    public String pay() {
+        return "pay";
+    }
+
+    @GetMapping("/goMarket")
+    public String market() {
+        return "market";
+    }
+
+    @GetMapping("/goHistory")
+    public String history() {
+        return "history";
+    }
+
+}
