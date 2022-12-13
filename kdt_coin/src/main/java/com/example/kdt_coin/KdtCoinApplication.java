6339b5c061b2762f3182c9e05bd2bb6bd126c12b
codingvegeta <113500967+codingvegeta@users.noreply.github.com>
2022-12-13 20:34:56 +0900

    Add files via upload

---
+++ kdt_coin/src/main/java/com/example/kdt_coin/KdtCoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.kdt_coin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtCoinApplication.class, args);
+    }
+
+}
---
+++ kdt_coin/src/test/java/com/example/kdt_coin/KdtCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kdt_coin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
