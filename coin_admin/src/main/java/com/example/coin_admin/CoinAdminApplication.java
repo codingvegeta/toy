e07365bee0b19ba2a178bdb3ad9ac1c75cd230e6
codingvegeta <hong090992@gmail.com>
2022-12-19 23:10:49 +0900

    2022/12/19 "feat: admin 페이지를 위한 기본 부트 프로그램 생성"

---
+++ coin_admin/src/main/java/com/example/coin_admin/CoinAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.coin_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinAdminApplication.class, args);
+	}
+
+}
---
+++ coin_admin/src/test/java/com/example/coin_admin/CoinAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coin_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
