bf40dba49958c5765d71e98c2e57deb016f7a462
codingvegeta <hong090992@gmail.com>
2022-12-20 00:58:17 +0900

    2022/12/19 "refactor: 페이지 컨트롤러의 위치를 변경하였습니다. "

--- coin_admin/src/main/java/com/example/coin_admin/admin/controller/PageController.java
+++ coin_admin/src/main/java/com/example/coin_admin/admin/controller/PageController.java
@@ -1,4 +1,4 @@
-package com.example.coin_admin.admin;
+package com.example.coin_admin.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
