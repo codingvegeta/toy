85e91a5601b15e5c07b25db44f7d7b5c50a94443
codingvegeta <hong090992@gmail.com>
2022-12-20 21:30:42 +0900

    2022/12/20 "feat : 페이지 컨트롤러를 작성하였습니다."

---
+++ kdt_cookie/src/main/java/com/example/kdt_cookie/page/controller/PageController.java
@@ -0,0 +1,46 @@
+package com.example.kdt_cookie.page.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PageController {
+
+//    @Autowired
+//    ProductService ps;
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+
+    @GetMapping("/signupPage")
+    public String signupPage() {
+        return "user/signup";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "user/login";
+    }
+
+    @GetMapping("/aboutus")
+    public String aboutus() {
+        return "aboutus";
+    }
+
+//    @GetMapping("/productList")
+//    public String productList(SearchDto params, Model model) {
+//        ProductList product = ps.findAll(params);
+//        model.addAttribute("product", product);
+//        String key=params.getKeyword();
+//        if(key!=null){
+//            model.addAttribute("keyword", params.getKeyword());
+//        }
+//
+//        return "productlist";
+//    }
+
+}
